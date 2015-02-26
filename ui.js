0430c695f8b17b87a54bc31baea5a0ebf1961048
legodude17 <joshuadb@me.com>
2015-02-25 20:20:04 -0800

    Removed the conflict

--- ui.js
+++ ui.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-function messageRecived(string){
-	console.log(string);
-return string;
-=======
 function messageRecived(string) {
   console.log(string);
   return string;
->>>>>>> origin/master
 }
 messageRecived('Hi!');
