0f97aaabb652149c59d2192b4ad34f711d292eac
legodude17 <joshuadb@me.com>
2015-02-25 20:13:48 -0800

    Removed socket.io
    I removed the socket.io parts

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-//var io = require('socket.io').listen(app);
-
-//io.sockets.on('connection', function (socket) {
-    //our other events...
-//});
-
 function sendMessage(message) {
   console.log("Message to send: " + message);
 }
