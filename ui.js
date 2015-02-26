069fbb77e3d9340bff5a712643085dadf54cd196
legodude17 <joshuadb@me.com>
2015-02-25 20:31:16 -0800

    Added prompting
    the ui now asks for user input

--- ui.js
+++ ui.js
@@ -1,5 +1,24 @@
+var prompt = require("prompt")
+prompt.start();
+function askForInput(ques){
+  prompt.get(['Input'], function (err, result) {
+    if (err) { return onErr(err); }
+    console.log('Command-line input received:');
+    console.log('You put in: ' + result
+    return result
+  });
+
+  function onErr(err) {
+    console.log(err);
+    return 1;
+  }
+
+  
+// Make a function to print the output of a message
 function messageRecived(string) {
-  console.log(string);
-  return string;
+//    log it to the console and return it
+   console.log(string);
+   return string;
 }
-messageRecived('Hi!');
+// Call it with 'hi' to test
+messageRecived(askForInput());
